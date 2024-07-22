b42480be2247d355709f17e4a1c5c02b6e3654b5
jimholcomb <33364519+jimholcomb@users.noreply.github.com>
2024-07-22 10:49:00 -0400

    Update update.sh
    Append to update.log

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo nala update && sudo nala upgrade -y
-echo "$(date)" > update.log
+echo "$(date)" >> update.log
 sudo nala autoremove -y
 if docker info >/dev/null 2>&1; then sudo docker system prune -f; fi
